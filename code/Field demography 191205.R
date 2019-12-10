6f66d34c56c1d63118a468296b7ffa5dfca521bd
Yan Yi Chung <yc68991@uga.edu>
2019-12-10 17:39:07 -0500

    Incorporated biomass estimation inside bootstrap to get better error estimates. Also corrected the upper percentile for 95% CI calculations.

--- code/Field demography 191205.R
+++ code/Field demography 191205.R
@@ -3,7 +3,6 @@
 
 setwd("C:/Users/yc68991/Box Sync/PSFcoexist/R/PSFcoexist")
 library(ggpubr)
-library(viridis)
 
 #Data import------------------------
 all2018<-read.csv("./data/Transplant data clean 190102.csv") #2018 data
@@ -25,46 +24,55 @@ estimates<-data.frame(Trans.Don.Treat=unique(all2018$Trans.Don.Treat),
                       mass.mean=rep(0,nrows),
                       mass.boot.mean=rep(0,nrows),
                       mass.boot.95CIlo=rep(0,nrows),
-                      mass.boot.95CIhi=rep(0,nrows))
-#Calculate bootstrap means and regular means
+                      mass.boot.95CIhi=rep(0,nrows),
+                      massContrib.mean=rep(0,nrows),
+                      massContrib.95CIlo=rep(0,nrows),
+                      massContrib.95CIhi=rep(0,nrows))
+
+
+#Calculate bootstrap means and regular means 
 nb<-5000
 for (i in 1:nrows){
   subdata<-subset(all2018,Trans.Don.Treat==estimates$Trans.Don.Treat[i])
   germ.boot.mean<-NULL
   surv.boot.mean<-NULL
   mass.boot.mean<-NULL
+  massContrib.boot.mean<-NULL
   for (j in 1:nb){
     germ.boot.mean[j]<-mean(sample(subdata$Germ,replace=T))
     surv.boot.mean[j]<-mean(sample(subdata$Surv,replace=T))
     mass.boot.mean[j]<-mean(sample(na.omit(subdata$Abv),replace=T))#Dead plants not included in mass calculation
+    massContrib.boot.mean[j]<-100*germ.boot.mean[j]*surv.boot.mean[j]*mass.boot.mean[j] #Directly estimated contributed mass shows strong kurtosis
   }
   estimates$germ.boot.mean[i]<-mean(germ.boot.mean)
   estimates$surv.boot.mean[i]<-mean(surv.boot.mean)
   estimates$mass.boot.mean[i]<-mean(mass.boot.mean)
+  estimates$massContrib.mean[i]<-mean(massContrib.boot.mean)
   estimates$germ.mean[i]<-mean(subdata$Germ)
   estimates$surv.mean[i]<-mean(subdata$Surv)
   estimates$mass.mean[i]<-mean(na.omit(subdata$Abv))
   estimates$germ.boot.95CIlo[i]<-quantile(germ.boot.mean,probs=0.025)
-  estimates$germ.boot.95CIhi[i]<-quantile(germ.boot.mean,probs=0.925)
+  estimates$germ.boot.95CIhi[i]<-quantile(germ.boot.mean,probs=0.975)
   estimates$surv.boot.95CIlo[i]<-quantile(surv.boot.mean,probs=0.025)
-  estimates$surv.boot.95CIhi[i]<-quantile(surv.boot.mean,probs=0.925)
+  estimates$surv.boot.95CIhi[i]<-quantile(surv.boot.mean,probs=0.975)
   estimates$mass.boot.95CIlo[i]<-quantile(mass.boot.mean,probs=0.025)
-  estimates$mass.boot.95CIhi[i]<-quantile(mass.boot.mean,probs=0.925)
+  estimates$mass.boot.95CIhi[i]<-quantile(mass.boot.mean,probs=0.975)
+  estimates$massContrib.95CIlo[i]<-quantile(massContrib.boot.mean,probs=0.025)
+  estimates$massContrib.95CIhi[i]<-quantile(massContrib.boot.mean,probs=0.975)
 }#Takes a few seconds
-#Bootstrap means pretty much approach the sample means, which I guess is expected. Not sure what to do with that just yet.
 
 #2018 estimated contribution to biomass-----------------
-estimates$massContrib.mean<-100*estimates$germ.boot.mean*estimates$surv.boot.mean*estimates$mass.boot.mean
-
-#Propagate errors
-estimates$massContrib.95CIlo<-sqrt(((estimates$germ.boot.mean-estimates$germ.boot.95CIlo)/estimates$germ.boot.mean)^2
-                                   +((estimates$surv.boot.mean-estimates$surv.boot.95CIlo)/estimates$surv.boot.mean)^2
-                                   +((estimates$mass.boot.mean-estimates$mass.boot.95CIlo)/estimates$mass.boot.mean)^2
-)
-estimates$massContrib.95CIhi<-sqrt(((estimates$germ.boot.mean-estimates$germ.boot.95CIhi)/estimates$germ.boot.mean)^2
-                                   +((estimates$surv.boot.mean-estimates$surv.boot.95CIhi)/estimates$surv.boot.mean)^2
-                                   +((estimates$mass.boot.mean-estimates$mass.boot.95CIhi)/estimates$mass.boot.mean)^2
-)
+#estimates$massContrib.mean<-100*estimates$germ.boot.mean*estimates$surv.boot.mean*estimates$mass.boot.mean
+#
+##Propagate errors
+#estimates$massContrib.95CIlo<-sqrt(((estimates$germ.boot.mean-estimates$germ.boot.95CIlo)/estimates$germ.boot.mean)^2
+#                                   +((estimates$surv.boot.mean-estimates$surv.boot.95CIlo)/estimates$surv.boot.mean)^2
+#                                   +((estimates$mass.boot.mean-estimates$mass.boot.95CIlo)/estimates$mass.boot.mean)^2
+#)
+#estimates$massContrib.95CIhi<-sqrt(((estimates$germ.boot.mean-estimates$germ.boot.95CIhi)/estimates$germ.boot.mean)^2
+#                                   +((estimates$surv.boot.mean-estimates$surv.boot.95CIhi)/estimates$surv.boot.mean)^2
+#                                   +((estimates$mass.boot.mean-estimates$mass.boot.95CIhi)/estimates$mass.boot.mean)^2
+#)
 
 #Visualize 2018 projections-----------------
 #Add labels back to data
@@ -83,7 +91,7 @@ estimates$Treatment <- factor(estimates$Treatment, levels=c("Control","Feedback"
 ggplot(data=estimates, aes(x=Type, y=massContrib.mean, color=Treatment, shape=Donorspp))+
   facet_grid(~Transplant)+
   geom_pointrange(aes(ymin=massContrib.mean-massContrib.95CIlo, ymax=massContrib.95CIhi+massContrib.mean)
-                  , position = position_dodge(width = 1))+  #Most errors are too small to see
+                  , position = position_dodge(width = 1))+  #Errors are giant now and very asymmetric
   scale_color_manual(values=c("#FF0000", "#00A08A", "#F2AD00"))+
   xlab("Microsite")+ylab("Estimated shoot biomass contribution from 100 seeds")+
   theme_bw()
@@ -104,7 +112,11 @@ estimates<-data.frame(Trans.Don.Treat=unique(all2019$Trans.Don.Treat),
                       mass.mean=rep(0,nrows),
                       mass.boot.mean=rep(0,nrows),
                       mass.boot.95CIlo=rep(0,nrows),
-                      mass.boot.95CIhi=rep(0,nrows))
+                      mass.boot.95CIhi=rep(0,nrows),
+                      massContrib.mean=rep(0,nrows),
+                      massContrib.95CIlo=rep(0,nrows),
+                      massContrib.95CIhi=rep(0,nrows))
+
 #Calculate bootstrap means and regular means
 nb<-5000
 for (i in 1:nrows){
@@ -112,38 +124,30 @@ for (i in 1:nrows){
   germ.boot.mean<-NULL
   surv.boot.mean<-NULL
   mass.boot.mean<-NULL
+  massContrib.boot.mean<-NULL
   for (j in 1:nb){
-    germ.boot.mean[j]<-mean(sample(na.omit(subdata$Germ),replace=T))
+    germ.boot.mean[j]<-mean(na.omit(sample(subdata$Germ,replace=T)))
     surv.boot.mean[j]<-mean(sample(subdata$Surv,replace=T))
     mass.boot.mean[j]<-mean(sample(na.omit(subdata$Abv),replace=T))#Dead plants not included in mass calculation
+    massContrib.boot.mean[j]<-100*germ.boot.mean[j]*surv.boot.mean[j]*mass.boot.mean[j] #Directly estimated contributed mass shows strong kurtosis
   }
   estimates$germ.boot.mean[i]<-mean(germ.boot.mean)
   estimates$surv.boot.mean[i]<-mean(surv.boot.mean)
   estimates$mass.boot.mean[i]<-mean(mass.boot.mean)
+  estimates$massContrib.mean[i]<-mean(massContrib.boot.mean)
   estimates$germ.mean[i]<-mean(na.omit(subdata$Germ))
   estimates$surv.mean[i]<-mean(subdata$Surv)
   estimates$mass.mean[i]<-mean(na.omit(subdata$Abv))
   estimates$germ.boot.95CIlo[i]<-quantile(germ.boot.mean,probs=0.025)
-  estimates$germ.boot.95CIhi[i]<-quantile(germ.boot.mean,probs=0.925)
+  estimates$germ.boot.95CIhi[i]<-quantile(germ.boot.mean,probs=0.975)
   estimates$surv.boot.95CIlo[i]<-quantile(surv.boot.mean,probs=0.025)
-  estimates$surv.boot.95CIhi[i]<-quantile(surv.boot.mean,probs=0.925)
+  estimates$surv.boot.95CIhi[i]<-quantile(surv.boot.mean,probs=0.975)
   estimates$mass.boot.95CIlo[i]<-quantile(mass.boot.mean,probs=0.025)
-  estimates$mass.boot.95CIhi[i]<-quantile(mass.boot.mean,probs=0.925)
+  estimates$mass.boot.95CIhi[i]<-quantile(mass.boot.mean,probs=0.975)
+  estimates$massContrib.95CIlo[i]<-quantile(massContrib.boot.mean,probs=0.025)
+  estimates$massContrib.95CIhi[i]<-quantile(massContrib.boot.mean,probs=0.975)
 }#Takes a few seconds
-#Bootstrap means pretty much approach the sample means
 
-#2019 estimated contribution to biomass-----------------
-estimates$massContrib.mean<-100*estimates$germ.boot.mean*estimates$surv.boot.mean*estimates$mass.boot.mean
-
-#Propagate errors
-estimates$massContrib.95CIlo<-sqrt(((estimates$germ.boot.mean-estimates$germ.boot.95CIlo)/estimates$germ.boot.mean)^2
-                                   +((estimates$surv.boot.mean-estimates$surv.boot.95CIlo)/estimates$surv.boot.mean)^2
-                                   +((estimates$mass.boot.mean-estimates$mass.boot.95CIlo)/estimates$mass.boot.mean)^2
-)
-estimates$massContrib.95CIhi<-sqrt(((estimates$germ.boot.mean-estimates$germ.boot.95CIhi)/estimates$germ.boot.mean)^2
-                                   +((estimates$surv.boot.mean-estimates$surv.boot.95CIhi)/estimates$surv.boot.mean)^2
-                                   +((estimates$mass.boot.mean-estimates$mass.boot.95CIhi)/estimates$mass.boot.mean)^2
-)
 
 #Visualize 2019 projections-----------------
 #Add labels back to data
@@ -162,7 +166,7 @@ estimates$Treatment <- factor(estimates$Treatment, levels=c("Control","Feedback"
 ggplot(data=estimates, aes(x=Type, y=massContrib.mean, color=Treatment, shape=Donorspp))+
   facet_grid(~Transplant)+
   geom_pointrange(aes(ymin=massContrib.mean-massContrib.95CIlo, ymax=massContrib.95CIhi+massContrib.mean)
-                  , position = position_dodge(width = 1))+  #All errors are too small to see
+                  , position = position_dodge(width = 1))+  #Errors asymmetric
   scale_color_manual(values=c("#FF0000", "#00A08A", "#F2AD00"))+
   xlab("Microsite")+ylab("Estimated shoot biomass contribution from 100 seeds")+
   theme_bw()
@@ -170,8 +174,10 @@ ggplot(data=estimates, aes(x=Type, y=massContrib.mean, color=Treatment, shape=Do
 #Alternative visualization
 ggplot(data=estimates, aes(x=Donorspp, y=massContrib.mean, color=Transplant, shape=Transplant))+
   facet_grid(~Treatment)+
-  geom_pointrange(aes(ymin=massContrib.mean-massContrib.95CIlo, ymax=massContrib.95CIhi+massContrib.mean))+  #All errors are too small to see
-  geom_line(aes(group=Transplant))+
+  geom_pointrange(aes(ymin=massContrib.mean-massContrib.95CIlo, ymax=massContrib.95CIhi+massContrib.mean),
+                  position = position_dodge(width = 0.5))+  
+  geom_line(aes(group=Transplant),position = position_dodge(width = 0.5))+
   scale_color_manual(values=c("#E2D200", "#46ACC8", "#E58601", "#00A08A"))+
   xlab("Microsite")+ylab("Estimated shoot biomass contribution from 100 seeds")+
-  theme_bw()
+  theme_bw()+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
